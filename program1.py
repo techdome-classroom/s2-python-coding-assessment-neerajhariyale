fed231e854a054257715edd179a5003d7c0e6347
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2024-10-07 14:40:47 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        d = {'()', '[]', '{}'}
+        for c in s:
+            if c in '({[':
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
+                return False
+        return not stk
